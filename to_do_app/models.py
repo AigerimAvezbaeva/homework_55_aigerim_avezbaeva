11e19bb6125044eb5a5705c87fe55ab3f4a7b25d
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-24 02:44:06 +0600

    добавила фиксацию времени по редактированию и удалению

--- to_do_app/models.py
+++ to_do_app/models.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -12,12 +14,26 @@ class ToDoParagraph(models.Model):
     description = models.TextField(max_length=2000, blank=False, null=True, verbose_name='Описание задачи')
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, blank=False,  default='new', verbose_name='Статус задачи')
     completion_date = models.DateField(verbose_name='Дата выполнения', blank=True, null=True)
+    is_deleted = models.BooleanField(verbose_name='Удалено', null=False, default=False)
+    deleted_at = models.DateTimeField(verbose_name='Дата и время удаления', null=True, default=None)
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата и время обновления")
+
 
     def __str__(self):
         return f'{self.title} - {self.status}'
 
-    @classmethod
-    def return_choices(cls):
-        statuses = dict(cls.STATUS_CHOICES)
-        return statuses
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 2:
+            raise ValidationError('Заголовок должен быть длиннее 2 символов')
+        return title
 
--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from to_do_app.views.base import to_do_view
-from to_do_app.views.delete_paragraph import delete_paragraph
-from to_do_app.views.add_paragraph import add_paragraph
-from to_do_app.views.add_paragraph import paragraph_view
+from to_do_app.views.to_do_view import add_paragraph, paragraph_view, update_toDo, delete_toDo, tdp_confirm_delete
 
 urlpatterns = [
     path('', to_do_view, name='index'),
     path('to_do_list/', to_do_view),
     path('to_do_list/add/', add_paragraph, name='add_paragraph'),
     path('to_do_list/<int:pk>', paragraph_view, name='paragraph_detail'),
-    path('to_do_list/delete/<int:pk>', delete_paragraph, name='delete_par')
+    path('to_do_list/<str:pk>/update/', update_toDo, name='todo_update'),
+    path('to_do_list/<int:pk>/delete/', delete_toDo, name='delete_par'),
+    path('to_do_list/<int:pk>/confirm_delete/', tdp_confirm_delete, name='confirm_delete')
 ]
