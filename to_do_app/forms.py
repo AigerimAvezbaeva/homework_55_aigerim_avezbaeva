b52089ae5d242f7b6e42c3f7da7a42bc04761c0f
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-24 02:45:11 +0600

    добавила класс формы

--- to_do_app/forms.py
+++ to_do_app/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import widgets
 from to_do_app.models import ToDoParagraph
 
 
-class ToDoForm(forms.Form):
+class ToDoForm(forms.ModelForm):
     class Meta:
         model = ToDoParagraph
-        fields = '__all__'
+        fields = ('title', 'description', 'status', 'completion_date')
 
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 2:
+            raise ValidationError('Заголовок должен быть длиннее 2 символов')
+        return title
